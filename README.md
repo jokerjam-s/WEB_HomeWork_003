b38345bfa061dc7ec8479902d5a0c47a8b32dc65
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-10-23 19:16:19 +0300

    Site ready

--- README.md
+++ README.md
@@ -5,5 +5,10 @@
 
 **Скриншоты**
 
+!["Скрин 1"](/ScreenShots/screen_01.png "Screen 1")
+!["Скрин 2"](/ScreenShots/screen_02.png "Screen 2")
+!["Скрин 3"](/ScreenShots/screen_03.png "Screen 3")
+!["Скрин 4"](/ScreenShots/screen_07.png "Screen 4")
+!["Скрин 5"](/ScreenShots/screen_06.png "Screen 5")
 
 ---
--- Site/myJS.js
+++ Site/myJS.js
@@ -1,5 +1,39 @@
-function askName(){
-    let userName = prompt("Ваше имя: ", "Кто вы?");
+function getInfo() {
+    let age, userName;
+    let nowYear = new Date();
 
-    document.getElementById("UserName").innerHTML = "Здравствуйте " + userName + "!";
+    userName = askData("Как ваше имя?", "Введите имя");
+    age = nowYear.getFullYear() - parseInt(askData("Год вашего рождения?", "2000"));
+
+    let descript = "Ваш возраст " + age + ". ";
+
+    if (age <= 0) {
+        descript +=
+            "Похоже вы еще не родились, вероятно мне пишут ваши будущие родители.";
+    } else if (age > 0 && age < 7) {
+        descript +=
+            "Вы ходите в детский сад. И я думаю весело проводите там время, а еще у вас много разных игрушек!";
+    } else if (age >= 7 && age <= 17) {
+        descript += "Вы школьник! Как успехи в учебе?";
+    } else if (age > 17 && age <= 24) {
+        descript +=
+        "Скорее всего вы студент, если так - поздравляю! От сессии до сессии живут студенты весело" + 
+        "А если вы парень - можеть быть вы служите в армии? В самоволку уже ходил? На 'губе' сидел?";
+    } else if (age > 24 && age < 65) {
+        descript += "Как работа, семья, дети?";
+    } else if (age >= 65 && age < 100) {
+        descript += "С заслуженным отдыхом! Или с внуками отдыхать некогда?";
+    } else {
+        descript +=
+        "Поздравляю, вы - долгожитель! Ну или просто мне врете про свой возраст :-)";
+    }
+
+    document.getElementById("UserName").innerHTML =
+        "Здравствуйте, " + userName + "!";
+    
+    document.getElementById("MainText").innerHTML = descript;
+}
+
+function askData(message, part) {
+    return prompt(message, part);
 }
